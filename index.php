c61c3ddfd6d16f80e6f1ba243a262519cb1161b6
André Ferreira <andrehrf@gmail.com>
2014-03-26 01:52:30 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,6 @@
      */
 
     require_once(__DIR__ . DIRECTORY_SEPARATOR . "bootstrap.php");
-    require_once(__DIR__ . DIRECTORY_SEPARATOR . "routes.php");
-    
     Bootstrap::Start();
     
     if(file_exists(__DIR__ . DIRECTORY_SEPARATOR . "routes.php"))
