9343e79a9396c06b8c3e83a564f86e4001d6afbe
MagicPHP <magicphp@magicphp.org>
2014-09-23 22:55:33 -0300

    Enabling display of errors for local development

--- index.php
+++ index.php
@@ -8,14 +8,19 @@
      * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
      */
 
+    if($_SERVER["REMOTE_ADDR"] == "127.0.0.1" || $_SERVER["REMOTE_ADDR"] == "::1"){
+        ini_set("display_errors", "on");
+        error_log(E_ERROR);
+    }
+
     if(file_exists(__DIR__ . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php"))
         require_once(__DIR__ . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php");
 
     require_once(__DIR__ . DIRECTORY_SEPARATOR . "bootstrap.php");
     Bootstrap::Start();
-    
+
     if(file_exists(__DIR__ . DIRECTORY_SEPARATOR . "routes.php"))
         require_once(__DIR__ . DIRECTORY_SEPARATOR . "routes.php"); 
-        
+
     Bootstrap::AutoLoad("settings");
     Routes::Parse();   
